3895d29e405b31e6c8ab37f1cf5ba493772f68dc
Omkar <omkarbhatuse895@example.com>
2024-07-09 20:18:16 +0530

    page6 animation

--- script.js
+++ script.js
@@ -36,41 +36,146 @@ ScrollTrigger.refresh();
 //     smooth: true
 // });
 
+function navAnimation() {
+  gsap.to("#nav-part1 svg", {
+    transform: "translateY(-100%)",
+    scrollTrigger: {
+      trigger: "#page1",
+      scroller: "#main",
+      start: "top 0",
+      end: "top -5%",
+      scrub: 2,
+    },
+  });
+
+  gsap.to("#nav-part2 #links", {
+    transform: "translateY(-100%)",
+    opacity: 0,
+    scrollTrigger: {
+      trigger: "#page1",
+      scroller: "#main",
+      start: "10% 15%",
+      end: "10% 15%",
+      scrub: 3,
+    },
+  });
+
+  gsap.from("#nav-part2 #links", {
+    transform: "translateY(-150%)",
+
+    duration: 1.5,
+    ease: "expoScale(0.5,7,none)",
+    stagger: 0.1, // Delay between each link animation
+  });
+}
+
+navAnimation();
+
 function LoadANimationh1() {
   gsap.from("#page1 h1", {
     y: 250,
     opacity: 0.1,
     scrub: 3,
-    duration: 1,
+    duration: 0.4,
   });
   gsap.from("#video-container", {
     scale: 0.8,
     opacity: 0,
     duration: 0.5,
-    delay: 1.2,
+    delay: 0.2,
     scrub: 1,
   });
 }
 LoadANimationh1();
 
-var crsr = document.querySelector("#cursor");
-var child1 = document.querySelector("#child1");
+// var crsr = document.querySelector("#cursor");
+// var child1 = document.querySelector("#child1");
 
-document.addEventListener("mousemove", function (dets) {
-  crsr.style.left = dets.x + "px";
-  crsr.style.top = dets.y + "px";
-});
+// document.addEventListener("mousemove", function (dets) {
+//   crsr.style.left = dets.x + "px";
+//   crsr.style.top = dets.y + "px";
+// });
 
-child1.addEventListener("mouseenter", function () {
-  gsap.to(crsr, {
-    transform: "translate(-50%,-50%) scale(1)",
+// child1.addEventListener("mouseenter", function () {
+//   gsap.to(crsr, {
+//     transform: "translate(-50%,-50%) scale(1)",
+//   });
+// });
+// child1.addEventListener("mouseleave", function () {
+//   gsap.to(crsr, {
+//     transform: "translate(-50%,-50%) scale(0)",
+//   });
+// });
+
+function customColorCursoronProduct() {
+  var crsr = document.querySelector("#cursor");
+  var children = document.querySelectorAll(".child");
+
+  // Track mouse movement
+  document.addEventListener("mousemove", function (event) {
+    crsr.style.left = event.clientX + "px";
+    crsr.style.top = event.clientY + "px";
   });
-});
-child1.addEventListener("mouseleave", function () {
-  gsap.to(crsr, {
-    transform: "translate(-50%,-50%) scale(0)",
+
+  // Loop through each child element
+  children.forEach(function (child) {
+    // Change cursor color and scale on hover
+    child.addEventListener("mouseenter", function () {
+      var childId = child.id;
+      switch (childId) {
+        case "child1":
+          gsap.to(crsr, {
+            backgroundColor: "#FBAB7E",
+            backgroundImage: "linear-gradient(62deg, #FBAB7E 0%, #F7CE68 100%)",
+            filter: "blur(10px)",
+            transform: "translate(-50%,-50%) scale(1)",
+          });
+
+          break;
+        case "child2":
+          gsap.to(crsr, {
+            backgroundColor: "#85FFBD",
+            backgroundImage: "linear-gradient(45deg, #85FFBD 0%, #FFFB7D 100%)",
+            filter: "blur(10px)",
+            transform: "translate(-50%,-50%) scale(1)",
+          });
+
+          break;
+        case "child3":
+          gsap.to(crsr, {
+            backgroundColor: "#A9C9FF",
+            backgroundImage:
+              "linear-gradient(180deg, #A9C9FF 0%, #FFBBEC 100%)",
+            filter: "blur(10px)",
+            transform: "translate(-50%, -50%) scale(1)",
+          });
+
+          break;
+        case "child4":
+          gsap.to(crsr, {
+            backgroundColor: "#d700e9",
+            backgroundImage: "linear-gradient(45deg, #d700e9 0%, #6923be 100%)",
+            filter: "blur(10px)",
+            transform: "translate(-50%,-50%) scale(1)",
+          });
+
+          break;
+        default:
+          gsap.to(crsr, {
+            backgroundColor: "black",
+            transform: "translate(-50%,-50%) scale(1)",
+          });
+      }
+    });
+
+    // Reset cursor on mouse leave
+    child.addEventListener("mouseleave", function () {
+      gsap.to(crsr, { transform: "translate(-50%,-50%) scale(0)" });
+    });
   });
-});
+}
+
+customColorCursoronProduct();
 
 function Video_Container_Animation() {
   var vdContainer = document.querySelector("#video-container");
@@ -131,126 +236,10 @@ function elem1Animation() {
 
 elem1Animation();
 
-// elem2 Animation
-
-function elem2Animation() {
-  var dets = document.querySelector(" #elem2 .dets");
-
-  dets.addEventListener("mouseenter", function () {
-    gsap.to("#elem2 .dets", {
-      height: "40vh",
-      duration: 0.5,
-      transformOrigin: "bottom",
-    });
-
-    gsap.to(" #elem2 .dets_Container img", {
-      opacity: 1,
-    });
-  });
-
-  dets.addEventListener("mouseleave", function () {
-    gsap.to(" #elem2 .dets_Container img", {
-      opacity: 0,
-    });
-    gsap.to("#elem2 .dets", {
-      height: "8vh",
-      duration: 0.5,
-    });
-  });
-}
-
-elem2Animation();
-
-// elem3 Animation
-
-function elem3Animation() {
-  var dets = document.querySelector(" #elem3 .dets");
-
-  dets.addEventListener("mousemove", function () {
-    gsap.to("#elem3 .dets", {
-      height: "40vh",
-      duration: 0.5,
-      transformOrigin: "bottom",
-    });
-
-    gsap.to(" #elem3 .dets_Container img", {
-      opacity: 1,
-    });
-  });
-
-  dets.addEventListener("mouseleave", function () {
-    gsap.to(" #elem3 .dets_Container img", {
-      opacity: 0,
-    });
-    gsap.to("#elem3 .dets", {
-      height: "8vh",
-      duration: 0.5,
-      ease: "expoScale(0.5,7,none)"
-    });
-  });
-}
-
-elem3Animation();
-
-
-
-
-
-gsap.to("#nav-part1 svg", {
-  
-  transform: "translateY(-100%)",
-   scrollTrigger: {
-    trigger: "#page1",
-    scroller: "#main",
-    start: "top 0",
-    end: "top -5%",
-    scrub: 2
-  },
-});
-
-
-// gsap.to("#nav-part1 svg", {
-  
-//   transform: "translateY(-100%)",
-//    scrollTrigger: {
-//     trigger: "#page1",
-//     scroller: "#main",
-//     start: "10% 15%",
-//     end: "10% 15%",
-//     scrub: 2
-//   },
-// });
-
-gsap.to("#nav-part2 #links", {
-  
-  transform: "translateY(-100%)",
-  opacity:0,
-   scrollTrigger: {
-    trigger: "#page1",
-    scroller: "#main",
-    start: "10% 15%",
-    end: "10% 15%",
-    scrub: 3
-  },
-});
-
-gsap.from("#nav-part2 #links", {
-  transform: "translateY(-150%)",
- 
-  duration:1.5,
-  ease: "expoScale(0.5,7,none)",
-  stagger: 0.1, // Delay between each link animation
-});
-
-
-
-
-function elem1ANimationResponsive(){
-
-
+function elem1ANimationResponsive() {
   function addAnimation() {
     var dets = document.querySelector("#elem1 .dets");
-  
+
     function mouseEnterHandler() {
       gsap.to("#elem1 .dets", {
         height: "40vh",
@@ -258,12 +247,12 @@ function elem1ANimationResponsive(){
         scrub: true,
         transformOrigin: "bottom",
       });
-  
+
       gsap.to("#elem1 .dets_Container img", {
         opacity: 1,
       });
     }
-  
+
     function mouseLeaveHandler() {
       gsap.to("#elem1 .dets_Container img", {
         opacity: 0,
@@ -273,40 +262,416 @@ function elem1ANimationResponsive(){
         duration: 0.3,
       });
     }
-  
+
     dets.addEventListener("mouseenter", mouseEnterHandler);
     dets.addEventListener("mouseleave", mouseLeaveHandler);
-  
+
     // Clean up the event listeners if screen width is more than 600px
     function removeAnimation() {
       dets.removeEventListener("mouseenter", mouseEnterHandler);
       dets.removeEventListener("mouseleave", mouseLeaveHandler);
     }
-  
+
     return removeAnimation;
   }
-  
+
   // Function to check screen width and add/remove the event listener
   function checkScreenWidth() {
     const removeAnimation = addAnimation();
-  
+
     if (window.innerWidth <= 600) {
       addAnimation();
     } else {
       removeAnimation();
     }
   }
-  
+
   // Initial check on page load
   checkScreenWidth();
-  
+
   // Check screen width on window resize
   window.addEventListener("resize", checkScreenWidth);
-  
-
 }
 
+elem1ANimationResponsive();
 
-elem1ANimationResponsive()
+
+function elem2Animation() {
+  var dets = document.querySelector(" #elem2 .dets");
+
+  dets.addEventListener("mouseenter", function () {
+    gsap.to("#elem2 .dets", {
+      height: "40vh",
+      duration: 0.5,
+      scrub: true,
+      transformOrigin: "bottom",
+    });
+
+    gsap.to(" #elem2 .dets_Container img", {
+      opacity: 1,
+    });
+  });
+
+  dets.addEventListener("mouseleave", function () {
+    gsap.to(" #elem2 .dets_Container img", {
+      opacity: 0,
+    });
+    gsap.to("#elem2 .dets", {
+      height: "8vh",
+      duration: 0.3,
+    });
+  });
+}
+
+elem2Animation();
+
+function elem2ANimationResponsive() {
+  function addAnimation() {
+    var dets = document.querySelector("#elem2 .dets");
+
+    function mouseEnterHandler() {
+      gsap.to("#elem2 .dets", {
+        height: "40vh",
+        duration: 0.5,
+        scrub: true,
+        transformOrigin: "bottom",
+      });
+
+      gsap.to("#elem2 .dets_Container img", {
+        opacity: 1,
+      });
+    }
+
+    function mouseLeaveHandler() {
+      gsap.to("#elem2 .dets_Container img", {
+        opacity: 0,
+      });
+      gsap.to("#elem2 .dets", {
+        height: "4.5vh",
+        duration: 0.3,
+      });
+    }
+
+    dets.addEventListener("mouseenter", mouseEnterHandler);
+    dets.addEventListener("mouseleave", mouseLeaveHandler);
+
+    // Clean up the event listeners if screen width is more than 600px
+    function removeAnimation() {
+      dets.removeEventListener("mouseenter", mouseEnterHandler);
+      dets.removeEventListener("mouseleave", mouseLeaveHandler);
+    }
+
+    return removeAnimation;
+  }
+
+  // Function to check screen width and add/remove the event listener
+  function checkScreenWidth() {
+    const removeAnimation = addAnimation();
+
+    if (window.innerWidth <= 600) {
+      addAnimation();
+    } else {
+      removeAnimation();
+    }
+  }
+
+  // Initial check on page load
+  checkScreenWidth();
+
+  // Check screen width on window resize
+  window.addEventListener("resize", checkScreenWidth);
+}
+
+elem2ANimationResponsive();
 
 
+
+function elem3Animation() {
+  var dets = document.querySelector(" #elem3 .dets");
+
+  dets.addEventListener("mouseenter", function () {
+    gsap.to("#elem3 .dets", {
+      height: "40vh",
+      duration: 0.5,
+      scrub: true,
+      transformOrigin: "bottom",
+    });
+
+    gsap.to(" #elem3 .dets_Container img", {
+      opacity: 1,
+    });
+  });
+
+  dets.addEventListener("mouseleave", function () {
+    gsap.to(" #elem3 .dets_Container img", {
+      opacity: 0,
+    });
+    gsap.to("#elem3 .dets", {
+      height: "8vh",
+      duration: 0.3,
+    });
+  });
+}
+
+elem3Animation();
+
+function elem3ANimationResponsive() {
+  function addAnimation() {
+    var dets = document.querySelector("#elem3 .dets");
+
+    function mouseEnterHandler() {
+      gsap.to("#elem3 .dets", {
+        height: "40vh",
+        duration: 0.5,
+        scrub: true,
+        transformOrigin: "bottom",
+      });
+
+      gsap.to("#elem3 .dets_Container img", {
+        opacity: 1,
+      });
+    }
+
+    function mouseLeaveHandler() {
+      gsap.to("#elem3 .dets_Container img", {
+        opacity: 0,
+      });
+      gsap.to("#elem3 .dets", {
+        height: "4.5vh",
+        duration: 0.3,
+      });
+    }
+
+    dets.addEventListener("mouseenter", mouseEnterHandler);
+    dets.addEventListener("mouseleave", mouseLeaveHandler);
+
+    // Clean up the event listeners if screen width is more than 600px
+    function removeAnimation() {
+      dets.removeEventListener("mouseenter", mouseEnterHandler);
+      dets.removeEventListener("mouseleave", mouseLeaveHandler);
+    }
+
+    return removeAnimation;
+  }
+
+  // Function to check screen width and add/remove the event listener
+  function checkScreenWidth() {
+    const removeAnimation = addAnimation();
+
+    if (window.innerWidth <= 600) {
+      addAnimation();
+    } else {
+      removeAnimation();
+    }
+  }
+
+  // Initial check on page load
+  checkScreenWidth();
+
+  // Check screen width on window resize
+  window.addEventListener("resize", checkScreenWidth);
+}
+
+elem3ANimationResponsive();
+
+function Page5Animation() {
+  var tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#page5",
+      pin: true,
+      markers: true,
+      scroller: `#main`,
+      start: `20% 10%`,
+      end: `500% 50%`,
+      scrub: 2,
+    },
+  });
+
+
+  tl.to("#card-one",{
+       top: "45%"
+    
+    })
+    
+  tl.to("#card-two",{
+        top: "45%"
+      
+      })
+  tl.to("#card-three",{
+        top: "45%"
+      
+      })    
+
+      tl.to("#card-four",{
+        top: "45%"
+      
+      }) 
+      tl.to("#card-five",{
+        top: "45%"
+      
+      }) 
+      
+     
+
+
+// function Page5Animation() {
+//   var tl = gsap.timeline({
+//     scrollTrigger: {
+//       trigger: "#page5",
+//       pin: true,
+//       markers: true,
+//       scroller: `#main`,
+//       start: `20% 10%`,
+//       end: `700% 50%`,
+//       scrub: 2,
+//     },
+//   });
+
+//   tl.to(
+//     "#card-one",
+//     {
+//       top: "35%",
+//       height: "80vh",
+
+//     },"a")
+//     .to(
+//       "#card-one",
+//       {
+//         top: "45%",
+//         height: "70vh",
+//       },"a")
+//     .to(
+//       "#card-one",
+//       {
+//         top: "50%",
+//         width: "65%",
+//         height: "80vh",
+//       },"a")
+//     .to(
+//       "#card-two",
+//       {
+//         top: "35%",
+//         height: "80vh",
+
+//       },"b")
+//     .to(
+//       "#card-two",
+//       {
+//         top: "40%",
+//         height: "70vh",
+
+//       },"b")
+//     .to(
+//       "#card-two",
+//       {
+//         top: "50%",
+//         width: "65%",
+//         height: "80vh",
+//       },"b")
+//     .to(
+//       "#card-three",
+//       {
+//         top: "35%",
+//         height: "80vh",
+
+//       },"c")
+//     .to(
+//       "#card-three",
+//       {
+//         top: "40%",
+//         height: "70vh",
+
+//       },"c")
+//     .to(
+//       "#card-three",
+//       {
+//         top: "50%",
+//         width: "65%",
+//         height: "80vh",
+//       },"c" )
+//       .to(
+//         "#card-four",
+//         {
+//           left: "35%",
+//           height: "80vh",
+//           top:"50%",
+//           transform:"traslateY(-50%)"
+    
+//         },"d")
+//         .to(
+//           "#card-four",
+//           {
+//             left: "45%",
+//             height: "70vh",
+//             top:"50%",
+//             transform:"traslateY(-50%)"
+
+//           },"d")
+//         .to(
+//           "#card-four",
+//           {
+//             left: "50%",
+//             width: "65%",
+//             height: "80vh",
+//             top:"50%",
+//             transform:"traslateY(-50%)"
+
+//           },"d")
+//           .to(
+//             "#card-five",
+//             {
+//               left: "35%",
+//           height: "80vh",
+//           top:"50%",
+//           transform:"traslateY(-50%)"
+      
+//             },"e")
+//           .to(
+//             "#card-five",
+//             {
+//               left: "45%",
+//             height: "70vh",
+//             top:"50%",
+//             transform:"traslateY(-50%)"
+      
+//             },"e")
+//           .to(
+//             "#card-five",
+//             {
+//               left: "50%",
+//               width: "65%",
+//               height: "80vh",
+//               top:"50%",
+//               transform:"traslate(-50%,-50%)"
+//             },"e")
+//             .to(
+//               "#card-six",
+//               {
+//                 left: "145%",
+//                 height: "80vh",
+//                 top:"50%",
+//                 transform:"traslateY(-50%)"
+          
+//               },"f")
+//               .to(
+//                 "#card-six",
+//                 {
+//                   left: "75%",
+//                   height: "70vh",
+//                   top:"50%",
+//                   transform:"traslateY(-50%)"
+      
+//                 },"f")
+//               .to(
+//                 "#card-six",
+//                 {
+//                   left: "50%",
+//                   width: "65%",
+//                   height: "80vh",
+//                   top:"50%",
+//                   transform:"traslateY(-50%)"
+      
+//                 },"f")
+}
+
+Page5Animation();
